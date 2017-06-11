5042a4ad8052e8f3b255b820265287d166351a9f
ivandev79 <ivandev79@gmail.com>
2017-06-11 20:01:27 +0300

    Fix lint error

--- html/index.js
+++ html/index.js
@@ -55,7 +55,7 @@ const Index = () => (
                     ))}
                 </div>
             </div>
-            <script dangerouslySetInnerHTML={{__html: `
+            <script dangerouslySetInnerHTML={{ __html: `
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -63,7 +63,8 @@ const Index = () => (
 
                 ga('create', 'UA-12418613-10', 'auto');
                 ga('send', 'pageview');
-            `}} />
+            ` }}
+            />
         </body>
     </html>
 );
